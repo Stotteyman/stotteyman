b9c0746f5d750f93b85907c5db5e1708b9370f14
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-22 20:05:48 -0800

    f

--- overlay/follower-goal/follower-goal.js
+++ overlay/follower-goal/follower-goal.js
@@ -1,10 +1,38 @@
+// Get the follower count element
+const followerCountElement = document.querySelector('.p-stream-title');
+
+// Get the progress bar element
+const progressBarElement = document.querySelector('.progress-bar');
+
+// Get the progress bar text element
+const progressBarTextElement = document.querySelector('.progress-bar-text');
+
+// Set the follower goal and current follower count
+const followerGoal = 75;
+let currentFollowers = parseInt(followerCountElement.textContent);
+
+// Update the progress bar fill and text
 function updateProgressBar() {
-  const followerCountElement = document.querySelector('p.stream-title');
-  const followerCount = parseInt(followerCountElement.innerText.replace(',', ''));
-  const progressBar = document.querySelector('.progress-bar');
-  const progressBarWidth = (followerCount / 75) * 100;
-  progressBar.style.width = `${progressBarWidth}%`;
-  document.querySelector('.progress-bar-text').innerText = `Follower Goal: ${followerCount} / 75`;
+  // Calculate the percentage of the goal that has been achieved
+  const percentage = Math.floor((currentFollowers / followerGoal) * 100);
+
+  // Set the width of the progress bar to the percentage achieved
+  progressBarElement.style.width = percentage + '%';
+
+  // Set the text of the progress bar to show the current and goal follower counts
+  progressBarTextElement.textContent = `Follower Goal: ${currentFollowers} / ${followerGoal}`;
 }
 
-setInterval(updateProgressBar, 5000); // check for new followers every 5 seconds
+// Call updateProgressBar initially
+updateProgressBar();
+
+// Check for updates to the follower count every 5 seconds
+setInterval(() => {
+  const newFollowers = parseInt(followerCountElement.textContent);
+
+  // Only update the progress bar if the follower count has changed
+  if (newFollowers !== currentFollowers) {
+    currentFollowers = newFollowers;
+    updateProgressBar();
+  }
+}, 5000);
