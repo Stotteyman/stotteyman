e9c018b122ffcc248819e9aec44f2ed1e5b36ba3
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-22 20:18:16 -0800

    Update follower-goal.js

--- overlay/follower-goal/follower-goal.js
+++ overlay/follower-goal/follower-goal.js
@@ -1,20 +1,38 @@
+// Get the follower count element
+const followerCountElement = document.querySelector('.p-stream-title');
+
+// Get the progress bar element
+const progressBarElement = document.querySelector('.progress-bar');
+
+// Get the progress bar text element
+const progressBarTextElement = document.querySelector('.progress-bar-text');
+
 // Set the follower goal and current follower count
 const followerGoal = 75;
-let currentCount = 36;
+let currentFollowers = parseInt(followerCountElement.textContent);
 
-// Get the progress bar and text elements
-const progressBar = document.getElementById("progress-bar");
-const followerCount = document.getElementById("follower-count");
-const progressText = document.getElementById("progress-text");
+// Update the progress bar fill and text
+function updateProgressBar() {
+  // Calculate the percentage of the goal that has been achieved
+  const percentage = Math.floor((currentFollowers / followerGoal) * 100);
 
-// Set the progress text to the current follower count
-followerCount.textContent = currentCount;
+  // Set the width of the progress bar to the percentage achieved
+  progressBarElement.style.width = percentage + '%';
 
-// Calculate the progress percentage
-const progressPercent = (currentCount / followerGoal) * 100;
+  // Set the text of the progress bar to show the current and goal follower counts
+  progressBarTextElement.textContent = `Follower Goal: ${currentFollowers} / ${followerGoal}`;
+}
 
-// Update the progress bar and text
-progressBar.style.color = "#FFA500";
-progressBar.style.width = progressPercent + "%";
-progressText.style.color = "#FFA500";
-progressText.textContent = "Follower Goal: " + currentCount + " / " + followerGoal;
+// Call updateProgressBar initially
+updateProgressBar();
+
+// Check for updates to the follower count every 5 seconds
+setInterval(() => {
+  const newFollowers = parseInt(followerCountElement.textContent);
+
+  // Only update the progress bar if the follower count has changed
+  if (newFollowers !== currentFollowers) {
+    currentFollowers = newFollowers;
+    updateProgressBar();
+  }
+}, 5000);
