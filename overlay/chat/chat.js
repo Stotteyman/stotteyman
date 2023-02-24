bb6cc54649aacfe62bf233557097c24eb5900825
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-24 13:26:11 -0800

    g

--- overlay/chat/chat.js
+++ overlay/chat/chat.js
@@ -1,11 +1,50 @@
-// Show/hide chat overlay
-var chatOverlay = document.getElementById("chat-overlay");
-var hideChatBtn = document.getElementById("hide-chat");
-
-hideChatBtn.addEventListener("click", function() {
-  chatOverlay.classList.add("hide");
-});
-
-setTimeout(function() {
-  chatOverlay.style.display = "flex";
-}, 5000);
+// Apply CSS to chat iframe
+var chatIframe = document.getElementById("chat-iframe");
+chatIframe.onload = function() {
+  var iframeHead = chatIframe.contentDocument.head;
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode(`
+    .h-full {
+      background-color: transparent !important;
+    }
+    .bg-secondary-dark {
+      background-color: transparent !important;
+    }
+    .border-secondary-lighter {
+      border-color: transparent !important;
+    }
+    .dark .dark\\:bg-secondary {
+      background-color: transparent !important;
+    }
+    body {
+      --tw-text-opacity: 1;
+      color: white !important;
+    }
+    .text-lg {
+      visibility: hidden !important;
+    }
+    .chat-input {
+      visibility: hidden !important;
+    }
+    .bg-secondary-lightest {
+      visibility: hidden !important;
+    }
+    .bg-secondary {
+      visibility: hidden !important;
+    }
+    .bg-secondary-lighter {
+      visibility: hidden !important;
+    }
+    .border-secondary-lighter {
+      border-color: transparent !important;
+    }
+    .bg-secondary-light {
+      visibility: hidden !important;
+    }
+    .border-secondary-light {
+      border-color: transparent !important;
+    }
+    .pt-3 {
+      padding-top: 0rem;
+    }
+    .line-clamp-
