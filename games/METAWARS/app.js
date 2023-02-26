1707599761c8d8e88f56a80528bda9c171d003d3
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-26 01:02:07 -0800

    yes

--- games/METAWARS/app.js
+++ games/METAWARS/app.js
@@ -1,13 +1,128 @@
 const network = "mumbai";
 const etherscanUrl = "https://mumbai.etherscan.io";
-const contractAddress = "0x9125531bCd55cF69C0187445E6ff5198bFD05E43";
+const contractAddress = "0x9125531bCd55cF69C0187445E6ff5198bFD05E43"; // replace with actual contract address
+
 const abi = [
-  "function addPlayer() public",
-  "function getPlayerCount() public view returns (uint256)",
-  "function getPlayerScore(address playerAddress) public view returns (uint256)",
-  "function setPlayerScore(address playerAddress, uint256 score) public",
-  "function setWinner(address winner) public",
+	{
+		"inputs": [],
+		"name": "addPlayer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "score",
+				"type": "uint256"
+			}
+		],
+		"name": "setPlayerScore",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			}
+		],
+		"name": "setWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayerCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getPlayerScore",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "playerAddresses",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "playerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "score",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "hasClaimed",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
+
 const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.matic.today");
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
