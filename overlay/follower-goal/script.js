eceaac0b35677f48a09b87114f125051c3090152
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-27 03:36:27 -0800

    Update script.js

--- overlay/follower-goal/script.js
+++ overlay/follower-goal/script.js
@@ -1,4 +1,4 @@
-const apiEndpoint = 'https://kick.com/api/v1/channels/stotteyman';
+const apiEndpoint = 'https://kick.com/api/v1/channels/deanza';
 const followerGoal = 200;
 
 function updateFollowerCount() {
