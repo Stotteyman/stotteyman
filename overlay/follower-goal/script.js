beb6cd8e331b6e9cd357ca42b8f5b9fff8664449
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-02-27 02:34:31 -0800

    d

--- overlay/follower-goal/script.js
+++ overlay/follower-goal/script.js
@@ -1,11 +1,14 @@
-let goal = document.getElementById("goal");
+let goal = document.querySelector(".progress-text");
+let progressBar = document.querySelector(".progress-bar");
 
 function updateFollowerCount() {
-    fetch("https://api.example.com/follower-count") // replace with the actual URL
+    fetch("https://kick.com/api/v1/channels/stotteyman")
     .then(response => response.json())
     .then(data => {
         let currentFollowers = data.followersCount;
-        goal.innerHTML = currentFollowers + "/200";
+        let percentComplete = currentFollowers / 2;
+        goal.innerHTML = currentFollowers + "/200 (" + percentComplete + "%)";
+        progressBar.style.width = percentComplete + "%";
     })
     .catch(error => console.log(error));
 }
