74b71f5f2806a8c1e1479eb6115d66dece1e56ec
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-01 16:10:57 -0800

    f

---
+++ overlay/follower-goal/expoze/script.js
@@ -0,0 +1,18 @@
+const apiEndpoint = 'https://kick.com/api/v1/channels/retal';
+const followerGoal = 75;
+
+function updateFollowerCount() {
+  fetch(apiEndpoint)
+    .then(response => response.json())
+    .then(data => {
+      const currentFollowers = data.followersCount;
+      const goalPercentage = Math.floor((currentFollowers / followerGoal) * 100);
+      document.getElementById('currentFollowers').textContent = currentFollowers;
+      document.getElementById('followerGoal').textContent = followerGoal;
+      document.querySelector('.progress-fill').style.width = `${goalPercentage}%`;
+    })
+    .catch(error => console.error('Error fetching follower count:', error));
+}
+
+updateFollowerCount();
+setInterval(updateFollowerCount, 60000); // Update every minute
