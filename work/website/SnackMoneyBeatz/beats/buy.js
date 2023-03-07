94dd886d02be4e4777ed9c6f668485afc50a2e89
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 21:12:25 -0800

    sd

--- work/website/SnackMoneyBeatz/beats/buy.js
+++ work/website/SnackMoneyBeatz/beats/buy.js
@@ -1,27 +1,29 @@
-// get all buy buttons
-const buyBtns = document.querySelectorAll('.buy-btn');
-
-// add click event listener to each buy button
-buyBtns.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    // get the beat info element for the current beat
-    const beatInfo = this.parentElement;
-    
-    // get the beat name and image for the current beat
-    const beatName = beatInfo.querySelector('h3').textContent;
-    const beatImg = beatInfo.parentElement.querySelector('.play-pause').src;
-    
-    // show the beat name and image in the popup
-    const popup = document.querySelector('.popup');
-    const popupImg = popup.querySelector('img');
-    const popupName = popup.querySelector('h2');
-    popupImg.src = beatImg;
-    popupName.textContent = beatName;
-    
-    // open the buy link for the current beat
-    const buyLink = this.dataset.buylink;
-    if (buyLink) {
-      window.open(buyLink, '_blank');
-    }
-  });
-});
+function buy() {
+    const buyBtns = document.querySelectorAll(".buy-btn");
+  
+    buyBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const audioSrc = btn.parentElement.previousElementSibling.getAttribute(
+          "data-audio"
+        );
+        const audioPlayer = document.getElementById("audio-player");
+        const audioName = btn.parentElement.querySelector("h3").textContent;
+        const popout = document.querySelector(".popup");
+  
+        audioPlayer.src = audioSrc;
+        audioPlayer.play();
+        popout.classList.add("active");
+        popout.querySelector("h2").textContent = audioName;
+  
+        const closeBtn = document.querySelector("#close-btn");
+        closeBtn.addEventListener("click", () => {
+          audioPlayer.pause();
+          audioPlayer.currentTime = 0;
+          popout.classList.remove("active");
+        });
+      });
+    });
+  }
+  
+  buy();
+  
