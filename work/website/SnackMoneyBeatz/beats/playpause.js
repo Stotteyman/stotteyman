e8239582adf26b423536edc0f1e4656c3010aca4
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 20:10:18 -0800

    Update playpause.js

--- work/website/SnackMoneyBeatz/beats/playpause.js
+++ work/website/SnackMoneyBeatz/beats/playpause.js
@@ -7,15 +7,28 @@ beats.forEach((beat) => {
 
   beatImg.addEventListener('click', () => {
     if (!isPlaying) {
+      // Pause any currently playing audio
+      stopAllAudio();
       audio = new Audio(beat.dataset.audio);
       audio.play();
       beat.classList.add('active');
       isPlaying = true;
     } else {
       audio.pause();
-      audio.currentTime = 0;
       beat.classList.remove('active');
       isPlaying = false;
     }
   });
 });
+
+function stopAllAudio() {
+  beats.forEach((beat) => {
+    const audio = beat.querySelector('audio');
+    const beatImg = beat.querySelector('img');
+    beatImg.classList.remove('active');
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+}
