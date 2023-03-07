f203be8f80ca538f93ed1d5bbde642e86d4c6e1f
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-07 00:14:20 -0800

    Update buy.js

--- work/website/SnackMoneyBeatz/beats/buy.js
+++ work/website/SnackMoneyBeatz/beats/buy.js
@@ -3,6 +3,7 @@ const buyButtons = document.querySelectorAll('.popup-buy-btn');
 buyButtons.forEach(button => {
   button.addEventListener('click', () => {
     const beatName = button.parentElement.parentElement.querySelector('h3').innerText;
-    window.location.href = `/buy/${beatName}`;
+    window.location.href = `
+    https://snackmoneybeatz.page.link/${beatName}`;
   });
 });
