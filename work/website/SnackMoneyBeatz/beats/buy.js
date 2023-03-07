2b14a4020d6111e48692c70effa0a1b3b1854a9f
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 21:08:13 -0800

    s

--- work/website/SnackMoneyBeatz/beats/buy.js
+++ work/website/SnackMoneyBeatz/beats/buy.js
@@ -1,13 +1,27 @@
-const buyButtons = document.querySelectorAll('.buy-btn');
-const popupTitle = document.querySelector('#popup-title');
-const popupLink = document.querySelector('#popup-link');
+// get all buy buttons
+const buyBtns = document.querySelectorAll('.buy-btn');
 
-buyButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    const beatInfo = this.closest('.beat-info');
-    const title = beatInfo.querySelector('h3').innerText;
-    const link = this.getAttribute('data-link');
-    popupTitle.innerText = title;
-    popupLink.href = link;
+// add click event listener to each buy button
+buyBtns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    // get the beat info element for the current beat
+    const beatInfo = this.parentElement;
+    
+    // get the beat name and image for the current beat
+    const beatName = beatInfo.querySelector('h3').textContent;
+    const beatImg = beatInfo.parentElement.querySelector('.play-pause').src;
+    
+    // show the beat name and image in the popup
+    const popup = document.querySelector('.popup');
+    const popupImg = popup.querySelector('img');
+    const popupName = popup.querySelector('h2');
+    popupImg.src = beatImg;
+    popupName.textContent = beatName;
+    
+    // open the buy link for the current beat
+    const buyLink = this.dataset.buylink;
+    if (buyLink) {
+      window.open(buyLink, '_blank');
+    }
   });
 });
