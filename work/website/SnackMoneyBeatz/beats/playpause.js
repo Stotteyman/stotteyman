362f595adc7a737110064cd8bf626f65fa96dbf3
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 23:41:55 -0800

    Update playpause.js

--- work/website/SnackMoneyBeatz/beats/playpause.js
+++ work/website/SnackMoneyBeatz/beats/playpause.js
@@ -1,16 +1,44 @@
+// Get all the play-pause buttons
 const playPauseButtons = document.querySelectorAll('.play-pause');
 
-playPauseButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    const audio = new Audio(this.dataset.audio);
-    const isPlaying = !audio.paused;
+// Keep track of the currently playing audio element
+let currentAudio = null;
 
-    if (isPlaying) {
-      audio.pause();
-    } else {
-      audio.play();
+// Loop through all the play-pause buttons and add a click event listener to each
+playPauseButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const audioSrc = button.getAttribute('data-audio');
+    const audioPlayer = document.getElementById('audio-player');
+    
+    // If the current audio element is playing and the clicked button is for the same audio file,
+    // pause the audio element and set the current audio element to null
+    if (currentAudio && currentAudio.getAttribute('src') === audioSrc) {
+      audioPlayer.pause();
+      currentAudio = null;
+      button.classList.toggle('playing');
+      return;
     }
 
-    this.classList.toggle('playing', !isPlaying);
+    // Pause the current audio element if there is one
+    if (currentAudio) {
+      audioPlayer.pause();
+      currentAudio.classList.toggle('playing');
+    }
+
+    // Set the audio source and play the audio
+    audioPlayer.setAttribute('src', audioSrc);
+    audioPlayer.play();
+
+    // Set the current audio element and add the playing class to the button
+    currentAudio = audioPlayer;
+    button.classList.toggle('playing');
   });
+});
+
+// Listen for the audio element to end and remove the playing class from the button
+document.getElementById('audio-player').addEventListener('ended', () => {
+  playPauseButtons.forEach(button => {
+    button.classList.remove('playing');
+  });
+  currentAudio = null;
 });
