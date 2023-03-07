e74fd8c4d7a6b77995d99db8ae53dd7867d1c4d9
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 21:52:18 -0800

    Update popout.js

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,39 +1,76 @@
-const popout = document.querySelector(".popup");
-const closeBtn = document.querySelector("#close-btn");
-const audioPlayer = document.querySelector("#audio-player");
-const popupBox = document.querySelector(".popup-box");
-const mediaBar = document.querySelector(".media-bar");
-const buyBtn = document.querySelector(".buy-btn");
+const popout = document.querySelector('.popout');
+const popoutMedia = document.querySelector('.popout-media');
+const popoutTitle = document.querySelector('.popout-title');
+const popoutMediaBar = document.querySelector('.popout-media-bar');
+const popoutCloseBtn = document.querySelector('.popout-close');
+const beats = document.querySelectorAll('.beat');
+const infoButtons = document.querySelectorAll('.info');
 
-function togglePopup() {
-  popout.classList.toggle("show-popup");
-  popupBox.classList.toggle("show-popup");
+// function to open the popout
+function openPopout(beat) {
+  const beatMedia = beat.querySelector('.beat-media');
+  const beatTitle = beat.querySelector('.beat-title');
+
+  popoutMedia.src = beatMedia.src;
+  popoutTitle.textContent = beatTitle.textContent;
+
+  popout.classList.add('open');
+  document.body.classList.add('no-scroll');
 }
 
-function closePopup() {
-  popout.classList.remove("show-popup");
-  popupBox.classList.remove("show-popup");
-  audioPlayer.pause();
+// function to close the popout
+function closePopout() {
+  popoutMedia.src = '';
+  popoutTitle.textContent = '';
+  popout.classList.remove('open');
+  document.body.classList.remove('no-scroll');
 }
 
-closeBtn.addEventListener("click", closePopup);
+// play/pause functionality
+beats.forEach((beat) => {
+  const playPauseBtn = beat.querySelector('.playpause');
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    closePopup();
+  playPauseBtn.addEventListener('click', () => {
+    if (beat.classList.contains('playing')) {
+      beat.classList.remove('playing');
+      playPauseBtn.innerHTML = 'play_circle_outline';
+    } else {
+      beats.forEach((otherBeat) => {
+        otherBeat.classList.remove('playing');
+        otherBeat.querySelector('.playpause').innerHTML = 'play_circle_outline';
+      });
+
+      beat.classList.add('playing');
+      playPauseBtn.innerHTML = 'pause_circle_outline';
+    }
+  });
+});
+
+// open popout when info button is clicked
+infoButtons.forEach((infoButton) => {
+  infoButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const beat = event.target.closest('.beat');
+    openPopout(beat);
+  });
+});
+
+// close popout when close button is clicked
+popoutCloseBtn.addEventListener('click', () => {
+  closePopout();
+});
+
+// close popout when escape key is pressed
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && popout.classList.contains('open')) {
+    closePopout();
   }
 });
 
-buyBtn.addEventListener("click", function () {
-  const audioSrc = audioPlayer.getAttribute("src");
-  window.location.href = "https://www.google.com/search?q=" + audioSrc;
+// redirect to purchase page when buy button is clicked in popout
+const buyBtn = document.querySelector('.popout-buy');
+
+buyBtn.addEventListener('click', () => {
+  window.location.href = 'https://www.google.com';
 });
 
-document.querySelectorAll(".play-pause").forEach(function (button) {
-  button.addEventListener("click", function () {
-    audioPlayer.src = button.getAttribute("data-audio");
-    togglePopup();
-    audioPlayer.play();
-    mediaBar.style.display = "flex";
-  });
-});
