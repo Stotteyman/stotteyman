0cc9d0035b02ed653c36df972e202e5e58b9827a
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 23:47:16 -0800

    Update popout.js

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,93 +1,31 @@
-const audioElements = document.querySelectorAll('audio');
+// Get the popup and close button elements
+const popup = document.querySelector('.popup');
+const closeBtn = document.getElementById('close-btn');
 
-audioElements.forEach(audio => {
-  const playPauseBtn = audio.nextElementSibling;
-  const playPauseIcon = playPauseBtn.querySelector('i');
+// Get all the "Info" buttons
+const infoBtns = document.querySelectorAll('.buy-btn');
 
-  playPauseBtn.addEventListener('click', () => {
-    if (audio.paused) {
-      audio.play();
-      playPauseIcon.classList.remove('fa-play');
-      playPauseIcon.classList.add('fa-pause');
-    } else {
-      audio.pause();
-      playPauseIcon.classList.remove('fa-pause');
-      playPauseIcon.classList.add('fa-play');
-    }
-  });
-});
-
-const popout = document.querySelector('.popout');
-const popoutMedia = document.querySelector('.popout-media');
-const popoutTitle = document.querySelector('.popout-title');
-const popoutMediaBar = document.querySelector('.popout-media-bar');
-const popoutCloseBtn = document.querySelector('.popout-close');
-const beats = document.querySelectorAll('.beat');
-const infoButtons = document.querySelectorAll('.info');
-
-function openPopout(beat) {
-  const beatMedia = beat.querySelector('.beat-media');
-  const beatTitle = beat.querySelector('.beat-title');
-
-  popoutMedia.src = beatMedia.src;
-  popoutTitle.textContent = beatTitle.textContent;
-
-  popout.classList.add('open');
-  document.body.classList.add('no-scroll');
+// Function to show the popup
+function showPopup() {
+  popup.style.display = 'block';
 }
 
-function closePopout() {
-  popoutMedia.src = '';
-  popoutTitle.textContent = '';
-  popout.classList.remove('open');
-  document.body.classList.remove('no-scroll');
+// Function to hide the popup
+function hidePopup() {
+  popup.style.display = 'none';
 }
 
-beats.forEach((beat) => {
-  const playPauseBtn = beat.querySelector('.playpause');
-
-  playPauseBtn.addEventListener('click', () => {
-    if (beat.classList.contains('playing')) {
-      beat.classList.remove('playing');
-      playPauseBtn.innerHTML = 'play_circle_outline';
-    } else {
-      beats.forEach((otherBeat) => {
-        otherBeat.classList.remove('playing');
-        otherBeat.querySelector('.playpause').innerHTML = 'play_circle_outline';
-      });
-
-      beat.classList.add('playing');
-      playPauseBtn.innerHTML = 'pause_circle_outline';
-    }
-  });
+// Add click event listeners to all "Info" buttons
+infoBtns.forEach((btn) => {
+  btn.addEventListener('click', showPopup);
 });
 
-infoButtons.forEach((infoButton) => {
-  infoButton.addEventListener('click', (event) => {
-    event.stopPropagation();
-    const beat = event.target.closest('.beat');
-    openPopout(beat);
-  });
-});
+// Add click event listener to the close button
+closeBtn.addEventListener('click', hidePopup);
 
-popoutCloseBtn.addEventListener('click', () => {
-  closePopout();
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape' && popout.classList.contains('open')) {
-    closePopout();
+// Hide the popup when the user clicks anywhere outside of it
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    hidePopup();
   }
-});
-
-const buyBtn = document.querySelector('.popout-buy');
-const buyLinks = {
-  "beat1": "https://www.example.com/beat1",
-  "beat2": "https://www.example.com/beat2",
-  "beat3": "https://www.example.com/beat3"
-};
-
-buyBtn.addEventListener('click', () => {
-  const popoutTitleText = popoutTitle.textContent.toLowerCase().replace(/\s/g, '');
-  window.location.href = buyLinks[popoutTitleText];
 });
