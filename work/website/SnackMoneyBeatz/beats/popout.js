7eb2d5b8bf858a2275715f16d5f5375b582da6ec
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-07 00:25:53 -0800

    Update popout.js

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,64 +1,43 @@
-// Get the popup
-var popup = document.querySelector(".popup");
+// Get the popup and close button elements
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('#close-btn');
 
-// Get the close button
-var closeBtn = document.querySelector("#close-btn");
+// Get the popup media elements
+const popupImg = document.querySelector('.popup-play-pause');
+const popupAudio = document.querySelector('#audio-player');
 
-// Get all the play/pause buttons
-var playPauseBtns = document.querySelectorAll(".play-pause");
+// Get the popup info and buy now button elements
+const popupInfo = document.querySelector('.popup-beat-info');
+const popupBuyBtn = document.querySelector('.popup-buy-info .buy-btn');
 
-// Get the popup play/pause button
-var popupPlayPauseBtn = document.querySelector(".popup-play-pause");
+// Add click event listeners to all info buttons
+const infoBtns = document.querySelectorAll('.beat-info .buy-btn');
+infoBtns.forEach(function(infoBtn) {
+  infoBtn.addEventListener('click', function() {
+    // Get the beat info elements for the clicked button
+    const beat = infoBtn.closest('.beat');
+    const beatImg = beat.querySelector('.play-pause');
+    const beatAudioSrc = beatImg.getAttribute('data-audio');
+    const beatTitle = beat.querySelector('h3').textContent;
 
-// Get the audio player
-var audioPlayer = document.querySelector("#audio-player");
-
-// Add event listeners to all the play/pause buttons
-playPauseBtns.forEach(function (playPauseBtn) {
-  playPauseBtn.addEventListener("click", function () {
-    var audioFile = this.getAttribute("data-audio");
-
-    // Set the audio source and play the audio
-    audioPlayer.src = audioFile;
-    audioPlayer.play();
+    // Update the popup media and info elements with the clicked beat info
+    popupImg.setAttribute('src', beatImg.getAttribute('src'));
+    popupAudio.setAttribute('src', beatAudioSrc);
+    popupInfo.querySelector('h3').textContent = beatTitle;
 
     // Show the popup
-    popup.classList.add("show");
-
-    // Update the popup play/pause button
-    popupPlayPauseBtn.src = this.src;
-    popupPlayPauseBtn.setAttribute("data-audio", audioFile);
-
-    // Update the popup beat info
-    var beatInfo = this.parentElement.querySelector(".beat-info");
-    var popupBeatInfo = document.querySelector(".popup-beat-info");
-    popupBeatInfo.querySelector("h3").innerHTML = beatInfo.querySelector("h3").innerHTML;
+    popup.style.display = 'block';
   });
 });
 
-// Add event listener to the close button
-closeBtn.addEventListener("click", function () {
+// Add click event listener to close button
+closeBtn.addEventListener('click', function() {
   // Pause the audio and hide the popup
-  audioPlayer.pause();
-  popup.classList.remove("show");
+  popupAudio.pause();
+  popup.style.display = 'none';
 });
 
-// Add event listener to the popup play/pause button
-popupPlayPauseBtn.addEventListener("click", function () {
-  if (audioPlayer.paused) {
-    audioPlayer.play();
-    this.src = "pause.png";
-  } else {
-    audioPlayer.pause();
-    this.src = this.getAttribute("data-play-img");
-  }
-});
-
-// Add event listener to the audio player to update the popup play/pause button
-audioPlayer.addEventListener("play", function () {
-  popupPlayPauseBtn.src = "pause.png";
-});
-
-audioPlayer.addEventListener("pause", function () {
-  popupPlayPauseBtn.src = popupPlayPauseBtn.getAttribute("data-play-img");
+// Add click event listener to buy now button
+popupBuyBtn.addEventListener('click', function() {
+  // Add code to handle buy now button click event
 });
