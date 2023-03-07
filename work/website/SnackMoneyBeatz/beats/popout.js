91a2cb97bc007357229a0b59f947f3444479b70a
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-07 00:20:48 -0800

    Update popout.js

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,30 +1,64 @@
-const beats = document.querySelectorAll(".beat");
+// Get the popup
+var popup = document.querySelector(".popup");
 
-beats.forEach((beat) => {
-  const popup = document.querySelector(".popup");
-  const popupBox = document.querySelector(".popup-box");
-  const closeBtn = document.querySelector("#close-btn");
-  const popupPlayPause = document.querySelector(".popup-play-pause");
-  const popupAudioPlayer = document.querySelector("#audio-player");
-  const popupBuyBtn = document.querySelector(".popup-buy-btn");
-  const popupBeatInfo = document.querySelector(".popup-beat-info");
+// Get the close button
+var closeBtn = document.querySelector("#close-btn");
 
-  beat.addEventListener("click", () => {
+// Get all the play/pause buttons
+var playPauseBtns = document.querySelectorAll(".play-pause");
+
+// Get the popup play/pause button
+var popupPlayPauseBtn = document.querySelector(".popup-play-pause");
+
+// Get the audio player
+var audioPlayer = document.querySelector("#audio-player");
+
+// Add event listeners to all the play/pause buttons
+playPauseBtns.forEach(function (playPauseBtn) {
+  playPauseBtn.addEventListener("click", function () {
+    var audioFile = this.getAttribute("data-audio");
+
+    // Set the audio source and play the audio
+    audioPlayer.src = audioFile;
+    audioPlayer.play();
+
+    // Show the popup
     popup.classList.add("show");
-    popupPlayPause.setAttribute("src", beat.querySelector(".play-pause").getAttribute("src"));
-    popupAudioPlayer.setAttribute("src", beat.querySelector(".play-pause").getAttribute("data-audio"));
-    popupBeatInfo.querySelector("h3").innerHTML = beat.querySelector("h3").innerHTML;
-    popupBox.style.backgroundImage = `url(${beat.querySelector("img").getAttribute("src")})`;
-  });
 
-  closeBtn.addEventListener("click", () => {
-    popup.classList.remove("show");
-    popupAudioPlayer.pause();
-    popupAudioPlayer.currentTime = 0;
-  });
+    // Update the popup play/pause button
+    popupPlayPauseBtn.src = this.src;
+    popupPlayPauseBtn.setAttribute("data-audio", audioFile);
 
-  popupBuyBtn.addEventListener("click", () => {
-    const beatNumber = popupBeatInfo.querySelector("h3").innerHTML.split(" ")[1];
-    window.location.href = `https://snackmoneybeatz.page.link/${beatNumber}`;
+    // Update the popup beat info
+    var beatInfo = this.parentElement.querySelector(".beat-info");
+    var popupBeatInfo = document.querySelector(".popup-beat-info");
+    popupBeatInfo.querySelector("h3").innerHTML = beatInfo.querySelector("h3").innerHTML;
   });
+});
+
+// Add event listener to the close button
+closeBtn.addEventListener("click", function () {
+  // Pause the audio and hide the popup
+  audioPlayer.pause();
+  popup.classList.remove("show");
+});
+
+// Add event listener to the popup play/pause button
+popupPlayPauseBtn.addEventListener("click", function () {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    this.src = "pause.png";
+  } else {
+    audioPlayer.pause();
+    this.src = this.getAttribute("data-play-img");
+  }
+});
+
+// Add event listener to the audio player to update the popup play/pause button
+audioPlayer.addEventListener("play", function () {
+  popupPlayPauseBtn.src = "pause.png";
+});
+
+audioPlayer.addEventListener("pause", function () {
+  popupPlayPauseBtn.src = popupPlayPauseBtn.getAttribute("data-play-img");
 });
