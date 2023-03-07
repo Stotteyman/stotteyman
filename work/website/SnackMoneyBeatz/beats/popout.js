3c0b43cfd3693b75987ace2fae9be7eb2cb5af28
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 21:16:34 -0800

    ss

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,19 +1,34 @@
-const popups = document.querySelectorAll(".play-pause");
+const playPauseButtons = document.querySelectorAll('.play-pause');
+const audioPlayer = document.querySelector('#audio-player');
+const popup = document.querySelector('.popup');
+const popupBox = document.querySelector('.popup-box');
+const closeBtn = document.querySelector('#close-btn');
 
-popups.forEach((popup) => {
-  popup.addEventListener("click", () => {
-    const audioSrc = popup.getAttribute("data-audio");
-    const audioPlayer = document.querySelector("#audio-player");
-    audioPlayer.setAttribute("src", audioSrc);
-    const popout = document.querySelector(".popup");
-    popout.classList.toggle("show");
+playPauseButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const audioSrc = button.getAttribute('data-audio');
+    const beatName = button.nextElementSibling.firstElementChild.innerText;
+
+    audioPlayer.setAttribute('src', audioSrc);
+
+    popup.style.display = 'block';
+    popupBox.style.display = 'block';
+    document.querySelector('body').style.overflow = 'hidden';
+
+    const beatNameDiv = document.createElement('div');
+    beatNameDiv.innerText = beatName;
+
+    popupBox.insertBefore(beatNameDiv, audioPlayer);
   });
 });
 
-const closeBtn = document.querySelector("#close-btn");
-closeBtn.addEventListener("click", () => {
-  const popout = document.querySelector(".popup");
-  popout.classList.toggle("show");
-  const audioPlayer = document.querySelector("#audio-player");
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+  popupBox.style.display = 'none';
+  document.querySelector('body').style.overflow = 'auto';
+
+  const beatNameDiv = popupBox.firstElementChild;
+  popupBox.removeChild(beatNameDiv);
   audioPlayer.pause();
+  audioPlayer.currentTime = 0;
 });
