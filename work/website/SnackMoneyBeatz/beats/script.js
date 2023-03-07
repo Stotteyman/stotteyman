702ac19ad309dc8485672b0a6f08fcb53ad928e5
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 19:11:55 -0800

    Update script.js

--- work/website/SnackMoneyBeatz/beats/script.js
+++ work/website/SnackMoneyBeatz/beats/script.js
@@ -1,47 +1,57 @@
 const beats = [
-  {
-    name: 'Beat 1',
-    imageSrc: 'beat1.jpg',
-    audioSrc: 'beat1.mp3',
-    buyLink: 'https://www.google.com'
-  },
-  {
-    name: 'Beat 2',
-    imageSrc: 'beat2.jpg',
-    audioSrc: 'beat2.mp3',
-    buyLink: 'https://www.google.com'
-  },
-  {
-    name: 'Beat 3',
-    imageSrc: 'beat3.jpg',
-    audioSrc: 'beat3.mp3',
-    buyLink: 'https://www.google.com'
-  },
-  {
-    name: 'Beat 4',
-    imageSrc: 'beat4.jpg',
-    audioSrc: 'beat4.mp3',
-    buyLink: 'https://www.google.com'
-  }
-];
-
-const audioPlayers = document.querySelectorAll('.audio-player');
-const beatContainers = document.querySelectorAll('.beat');
-
-beatContainers.forEach((beatContainer, index) => {
-  const audioPlayer = audioPlayers[index];
-  const beatImage = beatContainer.querySelector('img');
-  const beatInfo = beatContainer.querySelector('.beat-info');
-  const buyNowButton = beatContainer.querySelector('.buy-now-btn');
-  const playIcon = beatContainer.querySelector('.play-icon');
-  const pauseIcon = beatContainer.querySelector('.pause-icon');
-
-  beatContainer.addEventListener('mouseover', () => {
-    playIcon.style.opacity = 1;
+    {
+      name: 'Beat 1',
+      imageSrc: 'beat1.jpg',
+      audioSrc: 'beat1.mp3',
+      buyLink: 'https://www.google.com'
+    },
+    {
+      name: 'Beat 2',
+      imageSrc: 'beat2.jpg',
+      audioSrc: 'beat2.mp3',
+      buyLink: 'https://www.google.com'
+    },
+    {
+      name: 'Beat 3',
+      imageSrc: 'beat3.jpg',
+      audioSrc: 'beat3.mp3',
+      buyLink: 'https://www.google.com'
+    },
+    {
+      name: 'Beat 4',
+      imageSrc: 'beat4.jpg',
+      audioSrc: 'beat4.mp3',
+      buyLink: 'https://www.google.com'
+    }
+  ];
+  
+  const audioPlayers = document.querySelectorAll('.audio');
+  const beatContainers = document.querySelectorAll('.beat');
+  
+  beatContainers.forEach((beatContainer, index) => {
+    const audioPlayer = audioPlayers[index];
+    const beatImage = beatContainer.querySelector('img');
+    const beatInfo = beatContainer.querySelector('.beat-info');
+    const buyNowButton = beatContainer.querySelector('.buy-btn');
+  
+    beatImage.addEventListener('click', () => {
+      if (audioPlayer.paused) {
+        audioPlayer.play();
+        beatContainer.classList.add('active');
+      } else {
+        audioPlayer.pause();
+        beatContainer.classList.remove('active');
+      }
+    });
+  
+    audioPlayer.addEventListener('ended', () => {
+      beatContainer.classList.remove('active');
+    });
+  
+    buyNowButton.addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
   });
-
-  beatContainer.addEventListener('mouseout', () => {
-    playIcon.style.opacity = 0;
-  });
-
-  beatContainer.addEventListener('click
+  
+  let currentIndex = 0;
+  
