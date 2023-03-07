409e17d69afe3e4907ae2120631797732fba8c80
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 20:55:46 -0800

    sss

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,23 +1,19 @@
-const beats = document.querySelectorAll('.beat');
+const popups = document.querySelectorAll(".play-pause");
 
-beats.forEach(beat => {
-  const audio = new Audio(beat.querySelector('img').getAttribute('data-audio'));
-  const playButton = beat.querySelector('.play-button');
-  const popout = beat.querySelector('.beat-popout');
-
-  // Toggle the popout box when the beat is clicked
-  beat.addEventListener('click', () => {
-    popout.classList.toggle('show');
+popups.forEach((popup) => {
+  popup.addEventListener("click", () => {
+    const audioSrc = popup.getAttribute("data-audio");
+    const audioPlayer = document.querySelector("#audio-player");
+    audioPlayer.setAttribute("src", audioSrc);
+    const popout = document.querySelector(".popup");
+    popout.classList.toggle("show");
   });
+});
 
-  // Play or pause the audio when the play button is clicked
-  playButton.addEventListener('click', () => {
-    if (audio.paused) {
-      audio.play();
-      playButton.classList.add('pause');
-    } else {
-      audio.pause();
-      playButton.classList.remove('pause');
-    }
-  });
+const closeBtn = document.querySelector("#close-btn");
+closeBtn.addEventListener("click", () => {
+  const popout = document.querySelector(".popup");
+  popout.classList.toggle("show");
+  const audioPlayer = document.querySelector("#audio-player");
+  audioPlayer.pause();
 });
