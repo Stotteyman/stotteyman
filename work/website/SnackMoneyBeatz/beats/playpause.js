95eb82ce69a4be51af3925127fa1c5d38b9e307a
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 20:25:13 -0800

    s

--- work/website/SnackMoneyBeatz/beats/playpause.js
+++ work/website/SnackMoneyBeatz/beats/playpause.js
@@ -1,32 +1,38 @@
-const beats = document.querySelectorAll('.beat');
+// playpause.js
+const audioElements = document.querySelectorAll("audio");
+const playPauseButtons = document.querySelectorAll(".play-pause");
 
-beats.forEach((beat) => {
-  const beatImg = beat.querySelector('img');
-  let isPlaying = false;
-  let audio;
-
-  beatImg.addEventListener('click', () => {
-    if (!isPlaying) {
-      stopAllBeats();
-      audio = new Audio(beat.dataset.audio);
-      audio.play();
-      beat.classList.add('active');
-      isPlaying = true;
-    } else {
-      audio.pause();
-      beat.classList.remove('active');
-      isPlaying = false;
-    }
-  });
-});
-
-function stopAllBeats() {
-  beats.forEach((beat) => {
-    const audio = new Audio(beat.dataset.audio);
-    if (beat.classList.contains('active')) {
-      beat.classList.remove('active');
+// Pause all audio elements except the one passed in as an argument
+function pauseAllAudioElements(except) {
+  audioElements.forEach(function(audio) {
+    if (audio !== except) {
       audio.pause();
       audio.currentTime = 0;
     }
   });
 }
+
+// Handle clicks on play/pause buttons
+playPauseButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    const audio = document.querySelector(`audio[src="${this.dataset.audio}"]`);
+
+    // Pause all audio elements except the one that was just clicked
+    pauseAllAudioElements(audio);
+
+    // Play or pause the audio element, depending on its current state
+    if (audio.paused) {
+      audio.play();
+      this.classList.add("playing");
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      this.classList.remove("playing");
+    }
+  });
+});
+
+// Stop all audio elements when the window is unloaded
+window.addEventListener("unload", function() {
+  pauseAllAudioElements();
+});
