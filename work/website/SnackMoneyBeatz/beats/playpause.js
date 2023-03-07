ee99fb4f9d8749efec2bd14f244a4e999c9668b3
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 20:28:07 -0800

    Update playpause.js

--- work/website/SnackMoneyBeatz/beats/playpause.js
+++ work/website/SnackMoneyBeatz/beats/playpause.js
@@ -1,38 +1,18 @@
-// playpause.js
-const audioElements = document.querySelectorAll("audio");
-const playPauseButtons = document.querySelectorAll(".play-pause");
-
-// Pause all audio elements except the one passed in as an argument
-function pauseAllAudioElements(except) {
-  audioElements.forEach(function(audio) {
-    if (audio !== except) {
-      audio.pause();
-      audio.currentTime = 0;
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    var playPauseBtns = document.querySelectorAll('.play-pause');
+    
+    playPauseBtns.forEach(function(btn) {
+      btn.addEventListener('click', function() {
+        var audioEl = new Audio(btn.getAttribute('data-audio'));
+        
+        if (audioEl.paused) {
+          audioEl.play();
+          btn.src = 'pause.png';
+        } else {
+          audioEl.pause();
+          btn.src = 'play.png';
+        }
+      });
+    });
   });
-}
-
-// Handle clicks on play/pause buttons
-playPauseButtons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    const audio = document.querySelector(`audio[src="${this.dataset.audio}"]`);
-
-    // Pause all audio elements except the one that was just clicked
-    pauseAllAudioElements(audio);
-
-    // Play or pause the audio element, depending on its current state
-    if (audio.paused) {
-      audio.play();
-      this.classList.add("playing");
-    } else {
-      audio.pause();
-      audio.currentTime = 0;
-      this.classList.remove("playing");
-    }
-  });
-});
-
-// Stop all audio elements when the window is unloaded
-window.addEventListener("unload", function() {
-  pauseAllAudioElements();
-});
+  
