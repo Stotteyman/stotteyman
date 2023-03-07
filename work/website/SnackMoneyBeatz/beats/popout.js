6ffae85d421dd125ba8ec399f922a294e214091e
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 21:23:56 -0800

    ss

--- work/website/SnackMoneyBeatz/beats/popout.js
+++ work/website/SnackMoneyBeatz/beats/popout.js
@@ -1,35 +1,39 @@
-const playButtons = document.querySelectorAll(".play-pause");
-const audioPlayer = document.querySelector("#audio-player");
-const popup = document.querySelector(".popup");
-const popupImage = document.querySelector(".popup-img");
-const popupTitle = document.querySelector(".popup-title");
-const popupBuyBtn = document.querySelector(".popup-buy-btn");
+const popout = document.querySelector(".popup");
 const closeBtn = document.querySelector("#close-btn");
+const audioPlayer = document.querySelector("#audio-player");
+const popupBox = document.querySelector(".popup-box");
+const mediaBar = document.querySelector(".media-bar");
+const buyBtn = document.querySelector(".buy-btn");
 
-playButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const audioFile = button.getAttribute("data-audio");
-    audioPlayer.setAttribute("src", audioFile);
+function togglePopup() {
+  popout.classList.toggle("show-popup");
+  popupBox.classList.toggle("show-popup");
+}
 
-    popupImage.setAttribute("src", button.getAttribute("src"));
-    popupTitle.textContent = button.nextElementSibling.querySelector("h3").textContent;
-    popupBuyBtn.setAttribute("href", button.nextElementSibling.querySelector(".buy-btn").getAttribute("data-buy-link"));
-
-    popup.classList.add("open");
-    audioPlayer.play();
-  });
-});
-
-closeBtn.addEventListener("click", () => {
-  popup.classList.remove("open");
+function closePopup() {
+  popout.classList.remove("show-popup");
+  popupBox.classList.remove("show-popup");
   audioPlayer.pause();
-  audioPlayer.currentTime = 0;
+}
+
+closeBtn.addEventListener("click", closePopup);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closePopup();
+  }
 });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    popup.classList.remove("open");
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
-  }
+buyBtn.addEventListener("click", function () {
+  const audioSrc = audioPlayer.getAttribute("src");
+  window.location.href = "https://www.google.com/search?q=" + audioSrc;
+});
+
+document.querySelectorAll(".play-pause").forEach(function (button) {
+  button.addEventListener("click", function () {
+    audioPlayer.src = button.getAttribute("data-audio");
+    togglePopup();
+    audioPlayer.play();
+    mediaBar.style.display = "flex";
+  });
 });
