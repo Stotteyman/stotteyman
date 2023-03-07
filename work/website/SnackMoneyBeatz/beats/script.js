3b41e8db1e90bcf65f001af6b89736f97aae5511
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-06 19:11:26 -0800

    Update script.js

--- work/website/SnackMoneyBeatz/beats/script.js
+++ work/website/SnackMoneyBeatz/beats/script.js
@@ -1,48 +1,47 @@
 const beats = [
-    {
-      name: 'Beat 1',
-      imageSrc: 'beat1.jpg',
-      audioSrc: 'beat1.mp3',
-      buyLink: 'https://www.google.com'
-    },
-    {
-      name: 'Beat 2',
-      imageSrc: 'beat2.jpg',
-      audioSrc: 'beat2.mp3',
-      buyLink: 'https://www.google.com'
-    },
-    {
-      name: 'Beat 3',
-      imageSrc: 'beat3.jpg',
-      audioSrc: 'beat3.mp3',
-      buyLink: 'https://www.google.com'
-    },
-    {
-      name: 'Beat 4',
-      imageSrc: 'beat4.jpg',
-      audioSrc: 'beat4.mp3',
-      buyLink: 'https://www.google.com'
-    }
-  ];
-  
-  const audioPlayers = document.querySelectorAll('.audio-player');
-  const beatContainers = document.querySelectorAll('.beat');
-  
-  beatContainers.forEach((beatContainer, index) => {
-    const audioPlayer = audioPlayers[index];
-    const beatImage = beatContainer.querySelector('img');
-    const beatInfo = beatContainer.querySelector('.beat-info');
-    const buyNowButton = beatContainer.querySelector('.buy-now-btn');
-    const playIcon = beatContainer.querySelector('.play-icon');
-    const pauseIcon = beatContainer.querySelector('.pause-icon');
-  
-    beatContainer.addEventListener('mouseover', () => {
-      playIcon.style.opacity = 1;
-    });
-  
-    beatContainer.addEventListener('mouseout', () => {
-      playIcon.style.opacity = 0;
-    });
-  
-    beatContainer.addEventListener('click
-  
+  {
+    name: 'Beat 1',
+    imageSrc: 'beat1.jpg',
+    audioSrc: 'beat1.mp3',
+    buyLink: 'https://www.google.com'
+  },
+  {
+    name: 'Beat 2',
+    imageSrc: 'beat2.jpg',
+    audioSrc: 'beat2.mp3',
+    buyLink: 'https://www.google.com'
+  },
+  {
+    name: 'Beat 3',
+    imageSrc: 'beat3.jpg',
+    audioSrc: 'beat3.mp3',
+    buyLink: 'https://www.google.com'
+  },
+  {
+    name: 'Beat 4',
+    imageSrc: 'beat4.jpg',
+    audioSrc: 'beat4.mp3',
+    buyLink: 'https://www.google.com'
+  }
+];
+
+const audioPlayers = document.querySelectorAll('.audio-player');
+const beatContainers = document.querySelectorAll('.beat');
+
+beatContainers.forEach((beatContainer, index) => {
+  const audioPlayer = audioPlayers[index];
+  const beatImage = beatContainer.querySelector('img');
+  const beatInfo = beatContainer.querySelector('.beat-info');
+  const buyNowButton = beatContainer.querySelector('.buy-now-btn');
+  const playIcon = beatContainer.querySelector('.play-icon');
+  const pauseIcon = beatContainer.querySelector('.pause-icon');
+
+  beatContainer.addEventListener('mouseover', () => {
+    playIcon.style.opacity = 1;
+  });
+
+  beatContainer.addEventListener('mouseout', () => {
+    playIcon.style.opacity = 0;
+  });
+
+  beatContainer.addEventListener('click
