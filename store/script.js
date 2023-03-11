0ed13f3e7ed987719bbf179d5e62290b539d90d4
Stotteyman <99083765+Stotteyman@users.noreply.github.com>
2023-03-10 22:58:24 -0800

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,16 +1,8 @@
-var stripe = Stripe('pk_live_51MipdfCYfgL2SDGtEr49qYrKHNPdBTC8Rru3EzwAgXMoVmNvLFpaiy6l3gQuXK0spPwJNgyRSvKGB4WxZVTt9SZ100zJMInUfu');
-document.querySelectorAll('button').forEach(function(button) {
+var buyButtons = document.querySelectorAll('.buy-btn');
+
+buyButtons.forEach(function(button) {
 	button.addEventListener('click', function(e) {
-		var buyButtonId = e.target.getAttribute('id');
-		var itemPrice = parseFloat(e.target.previousElementSibling.innerText.replace('$', ''));
-		stripe.redirectToCheckout({
-			items: [{sku: buyButtonId, quantity: 1}],
-			successUrl: 'https://stotteyman.com/success',
-			cancelUrl: 'https://stotteyman.com/canceled',
-		}).then(function (result) {
-			if (result.error) {
-				console.log(result.error.message);
-			}
-		});
+		var checkoutLink = e.target.getAttribute('data-checkout-link');
+		window.location.href = checkoutLink;
 	});
 });
