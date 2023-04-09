8a43386f03928aeec25ce17ae38af63cc18f9a44
Gary McCullouch <stotteyman@gmail.com>
2023-04-08 23:26:38 -0700

    Update script.js

--- WSN/script.js
+++ WSN/script.js
@@ -1,54 +1,22 @@
-const streamersList = document.querySelector('#streamers-list');
+const streamersList = document.getElementById("streamers-list");
 
-fetch('usernames.txt')
+// Retrieve the list of usernames from usernames.txt
+fetch("usernames.txt")
   .then(response => response.text())
-  .then(usernames => {
-    const namesArray = usernames.trim().split('\n');
-    const onlineStreams = [];
-    const offlineStreams = [];
-
-    namesArray.forEach(name => {
-      fetch(`https://kick.com/api/v2/channels/${name}`)
-        .then(response => response.json())
-        .then(channel => {
-          if (channel.is_live) {
-            onlineStreams.push(channel);
-          } else {
-            offlineStreams.push(channel);
-          }
-
-          // Sort online streamers by viewers
-          onlineStreams.sort((a, b) => b.viewers - a.viewers);
-
-          // Sort offline streamers by followers
-          offlineStreams.sort((a, b) => b.followers - a.followers);
-
-          // Clear the current list
-          streamersList.innerHTML = '';
-
-          // Add the online streamers to the list
-          onlineStreams.forEach(stream => {
-            const streamer = document.createElement('li');
-            const link = document.createElement('a');
-            link.href = `https://kick.com/${stream.username}`;
-            link.target = '_blank';
-            link.innerText = `${stream.username} - ${stream.viewers} viewers`;
-            streamer.appendChild(link);
-            streamersList.appendChild(streamer);
-          });
-
-          // Add the offline streamers to the list
-          offlineStreams.forEach(stream => {
-            const streamer = document.createElement('li');
-            const link = document.createElement('a');
-            link.href = `https://kick.com/${stream.username}`;
-            link.target = '_blank';
-            link.innerText = `${stream.username} - ${stream.followers} followers`;
-            streamer.appendChild(link);
-            document.querySelector('#streamers-offline').appendChild(streamer);
-          });
-        })
-        .catch(error => console.error(`Error fetching channel data for ${name}: ${error}`));
+  .then(data => {
+    // Split the text into an array of usernames
+    const usernames = data.split("\n").filter(username => username.trim() !== "");
+    // Sort the usernames alphabetically
+    usernames.sort();
+    // Loop through each username and add it to the list
+    usernames.forEach(username => {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.textContent = username;
+      link.href = `https://kick.com/${username}`;
+      link.target = "_blank";
+      listItem.appendChild(link);
+      streamersList.appendChild(listItem);
     });
   })
-  .catch(error => console.error(`Error fetching usernames: ${error}`));
+  .catch(error => console.error(error));
