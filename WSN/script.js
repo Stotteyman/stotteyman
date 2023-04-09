8560ae06bc22e5a9d446a8a925c4268dbe81b717
Gary McCullouch <stotteyman@gmail.com>
2023-04-08 22:35:40 -0700

    e

--- WSN/script.js
+++ WSN/script.js
@@ -1,67 +1,40 @@
-// Function to get the online status of a streamer
-async function getStreamerStatus(username) {
-    const apiUrl = `https://kick.com/api/v2/channels/${username}`;
-    const response = await fetch(apiUrl);
+const streamersList = document.getElementById('streamers-list');
+
+// Function to fetch data from API and display the streamers list
+async function displayStreamers() {
+  const response = await fetch('usernames.txt');
+  const usernames = await response.text();
+
+  const streamers = [];
+
+  // Loop through each username and fetch the data from API
+  for (const username of usernames.split('\n')) {
+    if (!username) continue;
+
+    const url = `https://kick.com/api/v2/channels/${username.trim()}`;
+    const response = await fetch(url);
     const data = await response.json();
-    if (data.is_live === true) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  // Function to sort streamers by last online status
-  async function sortStreamersByLastOnline(streamers) {
-    const statusPromises = streamers.map(getStreamerStatus);
-    const statuses = await Promise.all(statusPromises);
-    streamers.sort((a, b) => {
-      const statusA = statuses[streamers.indexOf(a)];
-      const statusB = statuses[streamers.indexOf(b)];
-      if (statusA === true && statusB === false) {
-        return -1;
-      } else if (statusA === false && statusB === true) {
-        return 1;
-      } else {
-        const apiUrlA = `https://kick.com/api/v2/channels/${a}`;
-        const apiUrlB = `https://kick.com/api/v2/channels/${b}`;
-        const responseA = await fetch(apiUrlA);
-        const responseB = await fetch(apiUrlB);
-        const dataA = await responseA.json();
-        const dataB = await responseB.json();
-        const lastLiveTimeA = new Date(dataA.last_live_at);
-        const lastLiveTimeB = new Date(dataB.last_live_at);
-        return lastLiveTimeB - lastLiveTimeA;
-      }
+
+    streamers.push({
+      name: data.username,
+      viewers: data.current_viewers,
+      lastOnline: new Date(data.last_online).getTime()
     });
   }
-  
-  // Function to display streamers in the HTML
-  async function displayStreamers(streamers) {
-    const streamersList = document.getElementById('streamers-list');
-    for (const streamer of streamers) {
-      const listItem = document.createElement('li');
-      const link = document.createElement('a');
-      link.textContent = streamer;
-      link.href = `https://kick.com/${streamer}`;
-      const isOnline = await getStreamerStatus(streamer);
-      if (isOnline === true) {
-        listItem.classList.add('online');
-      } else {
-        listItem.classList.add('offline');
-      }
-      listItem.appendChild(link);
-      streamersList.appendChild(listItem);
-    }
+
+  // Sort the streamers list by last online time
+  streamers.sort((a, b) => b.lastOnline - a.lastOnline);
+
+  // Display the streamers list
+  streamersList.innerHTML = '';
+  for (const streamer of streamers) {
+    const li = document.createElement('li');
+    li.innerText = `${streamer.name} (${streamer.viewers} viewers)`;
+    li.addEventListener('click', () => {
+      window.location.href = `https://kick.com/${streamer.name}`;
+    });
+    streamersList.appendChild(li);
   }
-  
-  // Load streamers from usernames.txt
-  async function loadStreamers() {
-    const response = await fetch('usernames.txt');
-    const text = await response.text();
-    const streamers = text.split('\n').filter(username => username !== '');
-    await sortStreamersByLastOnline(streamers);
-    await displayStreamers(streamers);
-  }
-  
-  loadStreamers();
-  
+}
+
+displayStreamers();
