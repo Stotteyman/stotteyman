c77bd70e585b5f8a8e15e15a50bfc436f91a286f
Gary McCullouch <stotteyman@gmail.com>
2023-04-08 22:57:46 -0700

    Update script.js

--- WSN/script.js
+++ WSN/script.js
@@ -4,28 +4,12 @@ async function displayStreamers() {
   const response = await fetch('usernames.txt');
   const usernames = await response.text();
 
-  const streamers = [];
-
-  for (const username of usernames.split('\n')) {
-    const url = `https://kick.com/api/v2/channels/${username}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    streamers.push({
-      name: data.channel.username,
-      viewers: data.channel.current_viewers
-    });
-  }
-
-  streamers.sort((a, b) => a.name.localeCompare(b.name));
+  const streamers = usernames.trim().split('\n');
 
   streamersList.innerHTML = '';
   for (const streamer of streamers) {
     const li = document.createElement('li');
-    li.innerText = `${streamer.name} (${streamer.viewers} viewers)`;
-    li.addEventListener('click', () => {
-      window.location.href = `https://kick.com/${streamer.name}`;
-    });
+    li.innerText = streamer;
     streamersList.appendChild(li);
   }
 }
