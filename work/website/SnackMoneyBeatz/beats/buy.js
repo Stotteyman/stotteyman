50ca35efd0db8b281815c429b463555dee1c3650
Gary McCullouch <stotteyman@gmail.com>
2023-04-20 15:45:22 -0700

    gg

--- work/website/SnackMoneyBeatz/beats/buy.js
+++
@@ -1,9 +0,0 @@
-const buyButtons = document.querySelectorAll('.popup-buy-btn');
-
-buyButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const beatName = button.parentElement.parentElement.querySelector('h3').innerText;
-    window.location.href = `
-    https://snackmoneybeatz.page.link/${beatName}`;
-  });
-});
