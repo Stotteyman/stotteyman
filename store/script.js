b480af6b67ed881c2ba26e73b9e01b48c23410d8
Gary McCullouch <stotteyman@gmail.com>
2023-06-17 12:06:16 -0700

    g

--- store/script.js
+++
@@ -1,8 +0,0 @@
-var buyButtons = document.querySelectorAll('.buy-btn');
-
-buyButtons.forEach(function(button) {
-	button.addEventListener('click', function(e) {
-		var checkoutLink = e.target.getAttribute('data-checkout-link');
-		window.location.href = checkoutLink;
-	});
-});
