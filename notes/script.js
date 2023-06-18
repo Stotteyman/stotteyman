b25e785db0ed87ece37e90d0a9d39ff0a7a54ded
Gary McCullouch <stotteyman@gmail.com>
2023-06-18 10:31:49 -0700

    d

---
+++ notes/script.js
@@ -0,0 +1,36 @@
+var currentNote = 1; // Starting note number
+var totalNotes = 7; // Total number of notes
+
+var noteTitleElement = document.querySelector('.note-title');
+var noteContentElement = document.querySelector('.note-content');
+var previousButton = document.getElementById('previousButton');
+var nextButton = document.getElementById('nextButton');
+
+// Function to display the current note
+function displayNote() {
+  // Get the note based on the currentNote variable
+  var note = getNote();
+
+  // Set the note title and content in the HTML
+  noteTitleElement.textContent = note.title;
+  noteContentElement.innerHTML = note.content;
+
+  // Disable/enable previous and next buttons based on the currentNote
+  previousButton.disabled = (currentNote === 1);
+  nextButton.disabled = (currentNote === totalNotes);
+}
+
+// Function to retrieve the note based on the currentNote
+function getNote() {
+  // Define the notes and their titles/content
+  var notes = [
+    {
+      title: 'Relationship with God',
+      content: 'The Bible emphasizes the importance of a personal relationship with God...'
+    },
+    // Add more notes here with title and content properties
+    {
+      title: 'Service and Generosity',
+      content: 'The Bible teaches men to serve others and show generosity...'
+    }
+ 
