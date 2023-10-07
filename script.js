0908d6cee9c3da6fbd63585642a91385cee59602
Gary McCullouch <stotteyman@gmail.com>
2023-10-07 07:18:44 -0700

    dd

--- script.js
+++ script.js
@@ -1,43 +1,10 @@
-const sections = document.querySelectorAll('.section');
-const container = document.querySelector('.sections');
-let currentSection = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a');
 
-document.addEventListener('wheel', (e) => {
-    if (e.deltaY > 0 && currentSection < sections.length - 1) {
-        currentSection++;
-    } else if (e.deltaY < 0 && currentSection > 0) {
-        currentSection--;
-    }
-    scrollToSection(currentSection);
-});
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowDown' && currentSection < sections.length - 1) {
-        currentSection++;
-    } else if (e.key === 'ArrowUp' && currentSection > 0) {
-        currentSection--;
-    }
-    scrollToSection(currentSection);
-});
-
-let touchStartY = 0;
-document.addEventListener('touchstart', (e) => {
-    touchStartY = e.touches[0].clientY;
-});
-
-document.addEventListener('touchmove', (e) => {
-    const touchEndY = e.touches[0].clientY;
-    if (touchEndY - touchStartY > 50 && currentSection > 0) {
-        currentSection--;
-        scrollToSection(currentSection);
-    } else if (touchStartY - touchEndY > 50 && currentSection < sections.length - 1) {
-        currentSection++;
-        scrollToSection(currentSection);
-    }
-});
-
-function scrollToSection(index) {
-    sections.forEach((section, i) => {
-        section.style.transform = `translateY(-${100 * (index - i)}%)`;
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default behavior of the links
+            // Your custom logic here
+        });
     });
-}
+});
