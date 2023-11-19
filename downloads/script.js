646aab2b031633b29d38dcb5569793a43ba3fd59
Gary McCullouch <stotteyman@gmail.com>
2023-11-19 12:59:06 -0800

    Update script.js

--- downloads/script.js
+++ downloads/script.js
@@ -3,17 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var downloadStreaming = document.getElementById('download-streaming');
     
     downloadMinecraft.addEventListener('click', function() {
-        var link = document.createElement('a');
-        link.href = 'https://www.mediafire.com/file/rcb5gu245pg71s8/Stotteyman%2527s_PvP_Pack_%25231.zip/file';
-        link.target = '_blank';
-        link.download = 'Stotteyman_PvP_Pack.zip'; // Specify the desired file name
-        link.click();
+        window.location.href = 'https://www.mediafire.com/file/rcb5gu245pg71s8/Stotteyman%2527s_PvP_Pack_%25231.zip/file';
     });
 
     downloadStreaming.addEventListener('click', function() {
-        var link = document.createElement('a');
-        link.href = 'https://drive.google.com/drive/folders/1IWigR07d3sYkUVJXgKpHUUIFxfK19YkN?usp=drive_link';
-        link.target = '_blank';
-        link.click();
+        window.location.href = 'https://drive.google.com/drive/folders/1IWigR07d3sYkUVJXgKpHUUIFxfK19YkN?usp=drive_link';
     });
 });
