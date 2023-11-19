4157774c8e69f63d73c0a9f19d4dd4a6b9ee3314
Gary McCullouch <stotteyman@gmail.com>
2023-11-19 14:03:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var downloads = document.getElementById('downloads');
-    var store = document.getElementById('store');
+    var content = document.getElementById('content');
+    var navigationLinks = document.querySelectorAll('.navigation a');
 
-    downloads.addEventListener('click', function() {
-        // Redirect to the downloads page or trigger a specific download action
-        window.location.href = '/downloads'; // Replace '/downloads' with your actual downloads page URL
-    });
+    // Function to load content based on URL
+    function loadContent(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                content.innerHTML = html;
+            })
+            .catch(error => console.error('Error:', error));
+    }
 
-    store.addEventListener('click', function() {
-        // Redirect to the store page or perform a specific action related to your store
-        window.location.href = '/store'; // Replace '/store' with your actual store page URL
+    // Event listener for navigation clicks
+    navigationLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default link behavior
+            var url = this.getAttribute('href');
+            loadContent(url);
+        });
     });
 });
