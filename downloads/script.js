7a11a9599edb4fb00f110d2b9e8507f988df7e3c
Gary McCullouch <stotteyman@gmail.com>
2023-11-19 13:41:59 -0800

    s

--- downloads/script.js
+++ downloads/script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var downloadMinecraft = document.getElementById('download-minecraft');
-    var downloadStreaming = document.getElementById('download-streaming');
-    
-    downloadMinecraft.addEventListener('click', function() {
-        window.location.href = 'https://download1076.mediafire.com/jyeyql57f7igqFZZnyVzHjJqlRkfj_G3sPfYbv3CiZwwuTo8llu2fFskqYigiDUY4Jt5XBhKYNZFkf-UNkxEAAJPOpCtxJSv56sklt8srefQH7XDtUEsGsrG5T8cZ_a61jg21oG3BysU34GZ3w7RCmCmhzOIy9Zb0B6fzNfc3bM/rcb5gu245pg71s8/Stotteyman%5C%27s+PvP+Pack+%231.zip';
+    var downloads = document.getElementById('downloads');
+    var store = document.getElementById('store');
+
+    downloads.addEventListener('click', function() {
+        // Redirect to the downloads page or trigger a specific download action
+        window.location.href = '/downloads'; // Replace '/downloads' with your actual downloads page URL
     });
 
-    downloadStreaming.addEventListener('click', function() {
-        window.location.href = 'https://drive.google.com/drive/folders/1IWigR07d3sYkUVJXgKpHUUIFxfK19YkN?usp=drive_link';
+    store.addEventListener('click', function() {
+        // Redirect to the store page or perform a specific action related to your store
+        window.location.href = '/store'; // Replace '/store' with your actual store page URL
     });
 });
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a');
+    var downloads = document.getElementById('downloads');
+    var store = document.getElementById('store');
 
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the default behavior of the links
-            // Your custom logic here
-        });
+    downloads.addEventListener('click', function() {
+        // Redirect to the downloads page or trigger a specific download action
+        window.location.href = '/downloads'; // Replace '/downloads' with your actual downloads page URL
+    });
+
+    store.addEventListener('click', function() {
+        // Redirect to the store page or perform a specific action related to your store
+        window.location.href = '/store'; // Replace '/store' with your actual store page URL
     });
 });
