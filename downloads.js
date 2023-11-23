29882416a3fa4f8364ad687ceef7e7f783eca6d9
Gary McCullouch <stotteyman@gmail.com>
2023-11-23 13:33:20 -0800

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var downloadStreaming = document.getElementById('download-streaming');
     
     downloadMinecraft.addEventListener('click', function() {
-        window.location.href = 'https://download1076.mediafire.com/jyeyql57f7igqFZZnyVzHjJqlRkfj_G3sPfYbv3CiZwwuTo8llu2fFskqYigiDUY4Jt5XBhKYNZFkf-UNkxEAAJPOpCtxJSv56sklt8srefQH7XDtUEsGsrG5T8cZ_a61jg21oG3BysU34GZ3w7RCmCmhzOIy9Zb0B6fzNfc3bM/rcb5gu245pg71s8/Stotteyman%5C%27s+PvP+Pack+%231.zip';
+        window.location.href = 'https://download1076.mediafire.com/jyeyql57f7igqFZZnyVzHjJqlRkfj_G3sPfYbv3CiZwwuTo8llu2fFskqYigiDUY4Jt5XBhKYNZFkf-UNkxEAAJPOpCtxJSv56sklt8srefQH7XDtUEsGsrG5T8cZ_a61jg21oG3BysU34GZ3w7RCmCmhzOIy9Zb0B6fzNfc3bM/rcb5gu245pg71s8/Stotteyman%5C%27s+PvP+Pack+%231.zip', '_blank';
     });
 
     downloadStreaming.addEventListener('click', function() {
-        window.location.href = 'https://drive.google.com/drive/folders/1IWigR07d3sYkUVJXgKpHUUIFxfK19YkN?usp=drive_link';
+        window.location.href = 'https://drive.google.com/drive/folders/1IWigR07d3sYkUVJXgKpHUUIFxfK19YkN?usp=drive_link', '_blank';
     });
 });
