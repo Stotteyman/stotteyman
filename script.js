3436220c0b75c644485bc2d008d44a490ecc936b
Gary McCullouch <stotteyman@gmail.com>
2024-01-11 14:35:16 -0800

    s

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all the grid items
     const gridItems = document.querySelectorAll('.grid-item');
 
-    // Loop through each grid item and attach a click event listener
     gridItems.forEach(function(item) {
         item.addEventListener('click', function() {
             // Open the corresponding page in a new tab based on the clicked item
-            if (item.querySelector('img').alt === 'W.A.G.E. Society') {
-                window.open('https://wagesociety.com', '_blank'); // Link to W.A.G.E. Society
-            } else if (item.querySelector('img').alt === 'Coming Soon') {
-                // Add the link for the second project if available
-                window.open('/'); // Example link
+            const link = item.getAttribute('data-link');
+            if (link) {
+                window.open(link, '_blank');
             }
-            // Add more conditions for other projects if needed
         });
     });
 });
