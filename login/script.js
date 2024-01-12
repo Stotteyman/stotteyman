093a21b1a88957079be6dbb6ba24744f9255b67f
Gary McCullouch <stotteyman@gmail.com>
2024-01-11 21:44:44 -0800

    s

---
+++ login/script.js
@@ -0,0 +1,11 @@
+function googleSignIn() {
+    gapi.auth2.getAuthInstance().signIn().then(onSignIn);
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail());
+    // You can now handle the user data as needed, like sending it to your server.
+}
