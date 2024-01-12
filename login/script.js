3d68036e5dd0f5b0852d3943379238d0195b1651
Gary McCullouch <stotteyman@gmail.com>
2024-01-11 22:05:34 -0800

    Update script.js

--- login/script.js
+++ login/script.js
@@ -9,3 +9,10 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail());
     // You can now handle the user data as needed, like sending it to your server.
 }
+
+// Ensure that the redirect_uri matches the one registered in the Google API Console
+gapi.auth2.init({
+    client_id: '232342949144-u4amlu55qr6of2ps53vptgec2dv4m388.apps.googleusercontent.com',
+    scope: 'profile email openid offline_access',
+    redirect_uri: 'https://stotteyman.com/members/'
+});
