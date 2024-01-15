c63572304cd56aca290a2fb3fabc2ffed8d57315
Gary McCullouch <stotteyman@gmail.com>
2024-01-15 04:32:58 -0800

    s

--- BCH/script.js
+++ BCH/script.js
@@ -17,34 +17,37 @@ document.addEventListener("DOMContentLoaded", function() {
       return span;
     };
   
-    // Iterate over each page link
-    pageLinks.forEach((link, index) => {
-      // Skip empty links
-      if (link.trim() !== '') {
-        // Create a new span element for each word or space
-        const span = createSpacesSpan(index === 0 ? 0 : 6); // No space before the first word
-        span.innerHTML += link.split('.')[0]; // Extract the word from the link
+    // Create the reactive set of text
+    const reactiveText = 'home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; socials&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; customize&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; shop&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; sales&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;other';
   
-        // Add event listeners to change the color on hover
-        span.addEventListener('mouseover', function() {
-          this.style.color = 'red';
-        });
-        span.addEventListener('mouseout', function() {
-          this.style.color = '';
-        });
+    // Split the reactive text into individual words
+    const words = reactiveText.split(/\s+/);
   
-        // Add click event listener to redirect to the corresponding page
-        span.addEventListener('click', function() {
-          window.location.href = link;
-        });
+    // Iterate over each word
+    words.forEach((word, index) => {
+      // Create a new span element for each word or space
+      const span = createSpacesSpan(index === 0 ? 0 : 6); // No space before the first word
+      span.innerHTML += word;
   
-        // Append the span element to the navigation element
-        navElement.appendChild(span);
+      // Add event listeners to change the color on hover
+      span.addEventListener('mouseover', function() {
+        this.style.color = 'red';
+      });
+      span.addEventListener('mouseout', function() {
+        this.style.color = '';
+      });
   
-        // Add an additional space after each word (except the last one)
-        if (index < pageLinks.length - 1) {
-          navElement.appendChild(createSpacesSpan(6));
-        }
+      // Add click event listener to redirect to the corresponding page
+      span.addEventListener('click', function() {
+        window.location.href = pageLinks[index];
+      });
+  
+      // Append the span element to the navigation element
+      navElement.appendChild(span);
+  
+      // Add an additional space after each word (except the last one)
+      if (index < words.length - 1) {
+        navElement.appendChild(createSpacesSpan(6));
       }
     });
   });
