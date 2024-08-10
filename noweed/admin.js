a4f6cbab35aa00532fd035e4dcf72c8b09258f8b
Gary McCullouch <stotteyman@gmail.com>
2024-08-10 12:02:06 -0700

    s

--- noweed/admin.js
+++
@@ -1,6 +0,0 @@
-function resetTimestamp() {
-    // Reset timestamp to current time
-    var timestamp = Math.floor(new Date().getTime() / 1000);
-    // Post the timestamp to the main page
-    window.opener.postMessage({ timestamp: timestamp }, '*');
-}
